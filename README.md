8048daa46c50254dd2c8e0108c98ea02a9657335
Marc Ortiz <marc.ortiz@lanaccess.com>
2024-02-19 16:24:13 +0100

    Stable diffusion in area drawn

--- README.md
+++ README.md
@@ -3,15 +3,17 @@
 This repository contains code to run a stable diffusion model that generates new images, given an existing image and a promp text.
 The software can be interacted via GUI or terminal, as shown in the picture below:
 
-![Alt text](./images_readme/example_interface.png)
+![Alt text](./images_readme/step3.png)
 
 The model not only changes the style of the input image, but is also to interpret what you request in the prompt and add it to the final image.
 
 
 ## Installation
-Install pytorch >= 2.0 with cuda support.
+Install pytorch >= 2.0 with **cuda support**.
 
-Install the requirements: `pip install -r requirements.txt` 
+Install Tkinter: `sudo apt-get install python-tk ` in Ubuntu.
+
+Create a conda environment: `conda env create -f environment.yml` 
 
 Add the location of the project to PYTHONPATH: \
 `export PYTHONPATH=$PYTHONPATH/path/to/project/folder`
@@ -21,18 +23,24 @@ Add the location of the project to PYTHONPATH: \
 Execute the script you'll find in `./src/gui/gui.py`. It will take a bit of time to open since it's loading the model. \
 ![Alt text](./images_readme/step1.png)
 
-#### Step 2: Click on the left side of the panel to load an image
-![Alt text](./images_readme/steep2.png)
+#### Step 2: Load an image
+![Alt text](./images_readme/step2.png)
 
-#### Step 3: Write a prompt and click button to run  inference
-![Alt text](./images_readme/step4.png)
+#### Step 3: Write a prompt and click the button to run  inference
+![Alt text](./images_readme/step3.png)
+
+#### Step 4: Select the area of the image to change
+You can also select a rectangle in the input image where you want the prompt to be applied. In the following example, we take a portrait of a person and change it to become a gladiator, with a background of a Roman city
+![Alt text](./images_readme/step5.png)
 
 #### Optional: Run without interface
-You can also run the code without a graphical interface. To do so, please execute the script in `./src/ai/inference.py` with the right arguments.
+You can also run the code without a graphical interface. To do so, please execute the script in `./src/ai/inference.py`.
 
-## Improvements
-* Select the region in the image that you want the style to be applied
-* Possibility to generate an image from scratch with the prompt
+## Room for improvement
+Here I list some improvements that could be done to the project:
+* Implement a metric that allows the user to select the best output of the generative model based on some threshold.
+* Use a segmentation model to detect better the contour of the parts of the image we want to change.
+* Have the option to generate an image from scratch with a prompt.
 
 ## Information
 **Author**: Marc Ortiz Torres \
--- src/gui/gui.py
+++ src/gui/gui.py
@@ -3,16 +3,19 @@
     Date: 19 Feb 2024
 """
 
-from ai.inference import StableDiffusion
+from src.ai.inference import StableDiffusion
 
-from PIL import Image, ImageTk
 import tkinter as tk
+import numpy as np
+from PIL import Image, ImageTk, ImageDraw, ImageOps
 from tkinter import filedialog as fd
 
 class Window:
 
     def __init__(self):
         self.engine = StableDiffusion()
+        self.rectangle = []
+        self.canvas_ids = []
 
         self.window = tk.Tk()
         self.window.geometry("900x400")
@@ -20,38 +23,43 @@ class Window:
 
         # Controls line 1
         self.control_frame_1 = tk.Frame(self.window)
-        self.control_frame_1.grid(row=0, columnspan=2, pady=2)
-        self.window.grid_columnconfigure(0, weight=1)
+        self.control_frame_1.grid(row=0, columnspan=2, sticky='ew')
 
         self.prompt_text_description = tk.Label(self.control_frame_1, text='Prompt: ')
         self.prompt_text_description.pack(side=tk.LEFT)
-        self.prompt_text = tk.Entry(self.control_frame_1)
+        self.prompt_text = tk.Entry(self.control_frame_1, width=60)
         self.prompt_text.pack(side=tk.LEFT, expand=True)
 
         self.inference_button = tk.Button(self.control_frame_1, text='Run inference')
         self.inference_button.pack(side=tk.LEFT, expand=True)
         self.inference_button.bind('<Button-1>', self.do_inference)
 
+        self.save_button = tk.Button(self.control_frame_1, text='Load image')
+        self.save_button.pack(side=tk.LEFT, expand=True)
+        self.save_button.bind('<Button-1>', self.load_input_image)
+
         self.save_button = tk.Button(self.control_frame_1, text='Save image')
-        self.save_button.pack(side=tk.LEFT)
+        self.save_button.pack(side=tk.LEFT, expand=True)
         self.save_button.bind('<Button-1>', self.save_file)
 
         # Input image
-        self.input_frame = tk.Frame(self.window, width=350, height=300)
+        self.input_frame = tk.Frame(self.window, width=450, height=400)
         self.input_frame.grid(row=1, column=0, sticky="nsew")
 
-        self.window.grid_rowconfigure(1, weight=1)
-        self.window.grid_columnconfigure(0, weight=1)
-
-        self.input_image_label = tk.Label(self.input_frame,
-                                          text='Click here to load an image',
-                                          borderwidth=2,
-                                          relief="groove")
+        self.input_image_label = tk.Canvas(self.input_frame,
+                                           borderwidth=2,
+                                           relief="groove",
+                                           height=400,
+                                           width=450)
         self.input_image_label.pack(fill=tk.BOTH, expand=True)
-        self.input_image_label.bind('<Button-1>', self.load_input_image)
+        self.input_image_label.bind('<Button-1>', self.draw_rectangle)
+
+        self.image_canvas = self.input_image_label.create_image(self.input_image_label.winfo_width()/10,
+                                                                self.input_image_label.winfo_height()/10,
+                                                                anchor=tk.NW)
 
         # Output image
-        self.output_frame = tk.Frame(self.window, width=350, height=300)
+        self.output_frame = tk.Frame(self.window, width=450, height=400)
         self.output_frame.grid(row=1, column=1, sticky="nsew")
 
         self.window.grid_rowconfigure(1, weight=1)
@@ -63,19 +71,28 @@ class Window:
                                            relief="groove")
         self.output_image_label.pack(fill=tk.BOTH, expand=True)
 
-
     def load_input_image(self, event):
         self.filename = fd.askopenfilename()
-        pilimage = Image.open(self.filename)
+        pilimage = Image.open(self.filename).resize((self.input_image_label.winfo_width(),
+                                                     self.input_image_label.winfo_height()))
         self.tkimage_input = ImageTk.PhotoImage(pilimage)
-        self.input_image_label.config(image=self.tkimage_input)
+        self.input_image_label.itemconfig(self.image_canvas, image=self.tkimage_input)
 
     def do_inference(self, event):
         prompt = self.prompt_text.get()
+        if len(self.rectangle) == 5:
+            mask = Image.fromarray(self.make_mask_from_rectangle())
+        else:
+            mask = None
+
         if prompt.isascii() and len(prompt):
-            image = Image.open(self.filename)
-            self.output = self.engine.run(image, prompt)[0]
-            self.tkimage_output = ImageTk.PhotoImage(self.output)
+            image = Image.open(self.filename).resize((self.output_frame.winfo_width(), self.output_image_label.winfo_height()))
+            output = self.engine.run(image, prompt)[0].resize((self.output_frame.winfo_width(), self.output_image_label.winfo_height()))
+            if mask is not None:
+                self.final_image = Image.composite(image, output, mask)
+            else:
+                self.final_image = output
+            self.tkimage_output = ImageTk.PhotoImage(self.final_image)
             self.output_image_label.config(image=self.tkimage_output)
         else:
             print('Prompt is not valid')
@@ -84,6 +101,28 @@ class Window:
         file = fd.asksaveasfile(mode='w', defaultextension=".png")
         if file:
             self.output.save(file)
+
+    def make_mask_from_rectangle(self):
+        img = Image.new('L', (self.output_frame.winfo_width(),  self.output_image_label.winfo_height()), 0)
+        ImageDraw.Draw(img).polygon(self.rectangle, outline=255, fill=255)
+        inverted_img = ImageOps.invert(img)
+        mask = np.array(inverted_img)
+        return mask
+
+    def draw_rectangle(self, event):
+
+        if len(self.rectangle) > 3:
+            self.rectangle.append(self.rectangle[0])
+            print(self.rectangle)
+        else:
+            self.rectangle.append((event.x, event.y))
+
+        if len(self.rectangle) > 1:
+            self.input_image_label.create_line(*self.rectangle[-1], *self.rectangle[-2], tags='rectangle')
+
+        if len(self.rectangle) > 5:
+            self.rectangle = []
+            self.input_image_label.delete('rectangle')
 
     def run(self):
         self.window.mainloop()
